ca984f4c5f5c9b7dae144dbfcee99bee133dc253
Momijiichigo <ichigomomiji436@gmail.com>
2019-04-06 16:10:24 -0700

    map api test folder

--- test_forMapAPI/test001.js
+++ test_forMapAPI/test001.js
@@ -2,3 +2,7 @@ let platform = new H.service.Platform({
     "app_id":"iyo4JfGPiGiUwO7Jwkei",
     "app_code":"pmt_9cbTvp15TTYMhDOB-w"
 })
+let defaultLayers = platform.createDefaultLayers();
+let map = new H.Map(
+    document.getElementById
+)
